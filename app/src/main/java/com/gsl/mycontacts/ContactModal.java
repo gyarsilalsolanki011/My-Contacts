ea417e718685d86fe86d6b24a789dd3729032417
Tiger <gslofficial39@gmail.com>
2024-05-04 21:36:22 +0530

    my Contacts updated

--- app/src/main/java/com/gsl/mycontacts/ContactModal.java
+++ app/src/main/java/com/gsl/mycontacts/ContactModal.java
@@ -4,6 +4,30 @@ public class ContactModal {
     int img;
     String name, number;
 
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
     public ContactModal(int image, String name, String number){
         this.name = name;
         this.number = number;
